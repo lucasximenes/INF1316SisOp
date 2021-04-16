95f8e46e917c5846fcacee2e5258a6be4e4fca45
Lucas <lximenes99@gmail.com>
2021-04-16 00:13:59 -0300

    hahaha

--- lab4/exer4.c
+++ lab4/exer4.c
@@ -2,13 +2,36 @@
 #include <signal.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]){
-    float num1, num2;
+void zeroHandler(int sinal);
+
+int main(int argc, char *argv[])
+{
+    void (*p)(int);
+    float num1, num2, sum, sub, mul, div;
+    p = signal(SIGFPE, zeroHandler);
     num1 = atof(argv[1]);
     num2 = atof(argv[2]);
-    printf("%.3f + %.3f =  %.3f\n", num1, num2, num1 + num2);
-    printf("%.3f - %.3f =  %.3f\n", num1, num2, num1 - num2);
-    printf("%.3f * %.3f =  %.3f\n", num1, num2, num1 * num2);
-    printf("%.3f / %.3f =  %.3f\n", num1, num2, num1 / num2);
+    sum = num1 + num2;
+    printf("%.3f + %.3f =  %.3f\n", num1, num2, sum);
+    sub = num1 - num2;
+    printf("%.3f - %.3f =  %.3f\n", num1, num2, sub);
+    mul = num1 * num2;
+    printf("%.3f * %.3f =  %.3f\n", num1, num2, mul);
+    if ((int)num2 == 0)
+    {
+        div = (int)num1 / (int)num2;
+        printf("%.3f / %.3f =  %.3f\n", num1, num2, div);
+    }
+    else
+    {
+        div = num1 / num2;
+        printf("%.3f / %.3f =  %.3f\n", num1, num2, div);
+    }
     return 0;
+}
+
+void zeroHandler(int sinal)
+{
+    printf("Você tentou dividir por 0!!!!!!, sig:(%d)\n", sinal);
+    exit(0);
 }
