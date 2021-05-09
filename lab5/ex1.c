87d38a5c359efc0d766207b08751dc4a9e61e422
Lucas <lximenes99@gmail.com>
2021-05-09 20:29:35 -0300

    inicio lab5

---
+++ lab5/ex1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/msg.h>
+#include "fila.h"
+
+int main(void)
+{
+    return 0;
+}
---
+++ lab5/fila.h
@@ -0,0 +1,8 @@
+typedef struct no No;
+typedef struct fila Fila;
+
+Fila* criaFila();
+void liberaFila();
+void liberaFila();
+void insereFila();
+int filaRetira();
