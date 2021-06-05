3f273d7f44cd1fd355f87b80b3844f5fab50c111
MiguelVuori <mfvuori@gmail.com>
2021-06-04 22:55:05 -0300

    base da simulacao

--- lab7/gerMem.c
+++ lab7/gerMem.c
@@ -4,30 +4,50 @@
 #include <math.h>
 #include <time.h>
 
-struct gerMem
+struct pagina
 {
     int R;
     int M;
-    time_t last_access;
+    short emMemoria;
+    time_t ultimoAcesso;
 };
 
-typedef struct gerMem GerMem;
-
+typedef struct pagina Pagina;
 
 int main (int argc, char * argv[]){
-    int tamPag, tamMem, time = 0;
+    int tamPag, tamMem, tempo = 0, res = 0;
     FILE * file;
-    char * rw;
-    unsigned int addr, page;
-    int numPags;
+    char * rw, * algoritmo;
+    unsigned int addr, page, bits, mAddr;
+    int numPags, numQuadros, *memoria;
+    time_t tinicial = time(NULL);
 
     if(argc == 5){
-        GerMem* tabela;
+        Pagina ** tabela;
+        algoritmo = argv[1];
         tamPag = atoi(argv[3]);
         tamMem = atoi(argv[4]);
-        numPags = (tamMem/tamPag) * 1000;
-        printf("\nnumpags = %d\n", numPags);
-        // tabela = (GerMem*)malloc(sizeof(GerMem)*numPags);
+
+        if(tamPag < 8 || tamPag > 32)
+        {
+            printf("Tamanho da pagina deve ter o valor entre 8 e 32");
+            return 0;
+        }
+
+        if(tamMem < 8 || tamMem > 32)
+        {
+            printf("Tamanho da memoria deve ter o valor entre 1 e 16");
+            return 0;
+        }
+        numQuadros = (tamMem/tamPag) * 1024;
+        memoria = (int *) malloc (numQuadros * sizeof(int)); // vetor que representa a memoria
+
+        // inicializacao da memoria, -1 representa a ausencia de pagina
+        for (int i = 0; i < numQuadros; i++) {
+            memoria[i] = -1;
+        }
+        printf("\nnumquadros = %d\n", numQuadros);
+
         file = fopen( argv[2] , "r");
         // if (file == NULL)
         //     exit(EXIT_FAILURE);
@@ -38,11 +58,87 @@ int main (int argc, char * argv[]){
         printf("Tamanho das paginas: %s KB\n", argv[3]);
         printf("Algoritmo de substituição: %s\n", argv[1]);
 
-        // while (fscanf(file, "%x %c", &addr, &rw))
-        // {
-        //     page = addr >> (int)(sqrt(tamPag) + 10);
-        // }
+
+        tamMem *= 1024; // passando de MB para KB
+        bits = 0; // bits menos significativos
+        mAddr = tamPag * 1024 - 1; // maximo de memoria
+
+        while (mAddr > 0) {
+            mAddr = mAddr >> 1;
+            bits++;
+        }
+
+        numPags = (int)pow(2.0,(double)(32 - bits));
+
+        printf("bits = %d\n",bits);
+        printf("1 : %d , 2 : %f\n", numPags, pow(2.0,(double)(32 - bits)));
+
+        // Cria tabela de paginas
+        tabela = (Pagina **) malloc(numPags * sizeof(Pagina *));
+
+        // Inicializa tabela
+        for (int i = 0; i < numPags; i++) {
+            Pagina *p = (Pagina *) malloc(sizeof(Pagina));
+            tabela[i] = p;
+            tabela[i]->R = 0;
+            tabela[i]->M = 0;
+            tabela[i]->emMemoria = 0;
+            tabela[i]->ultimoAcesso = 0;
+        }
         
+        // leitura do log de entrada
+        while ((res = fscanf(log, "%x %c ", &addr, &rw)) != EOF)
+        {
+            page = addr >> bits; // indice da pagina
+
+            // caso o algoritmo seja NRU reseta as flags a cada 1000 iteracoes
+            if (!strcmp(algoritmo,"LRU") && tempo % 1000)
+            {
+                for(int i = 0; i < numQuadros; i++)
+                {
+                    if(memoria[i] != -1 && (tabela[memoria[i]]->R))
+                    {
+                        tabela[memoria[i]]->R = 0;
+                    }
+                }
+            }
+
+            // a pagina nao esta na memoria
+            if(!tabela[page]->emMemoria)
+            {
+                // chama o algoritmo de substituicao de pagina correspondente
+
+                if(!strcmp(algoritmo,"LRU"))
+                {
+
+                }
+                else if (!strcmp(algoritmo,"NRU"))
+                {
+
+                }
+                else if (!strcmp(algoritmo,"2Chance"))
+                {
+
+                }
+
+                tabela[page]->R = 1;
+                if(rw == "W")
+                {
+                    tabela[page]->M = 1;
+                }
+            }
+
+            tempo++;
+
+            
+        }
+
+        // liberacao da memoria dinamica
+        for (int i = 0; i < numPags; i++) {
+            free(tabela[i]);
+        }
+        free(tabela);
+        free(memoria);
 
     }
     else{
